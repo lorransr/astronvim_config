ccb83feec35c27642f63b711314137d3b00cddfb
lorransr <lorran.rodr@gmail.com>
2023-09-28 18:16:45 -0300

    add community python pack

---
+++ plugins/community.lua
@@ -0,0 +1,12 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
+    },
+  },
+}
